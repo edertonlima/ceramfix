673e79e1c1056bb4c486a0ee992687ed609a4107
Ederton Lima <contato@ederton.com.br>
2017-01-23 17:22:48 -0200

    Finalização Home

--- includes/head.php
+++ includes/head.php
@@ -18,26 +18,14 @@
 
 <title></title>
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
 <!-- CSS -->
 <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="screen" />
 
 <!-- JQUERY -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-<?php /*
-<!-- CSS SELECT2-->
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/select2.min.css" media="screen" />
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/select2.min.js"></script>
-
-<!-- FILE STYLE -->
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-filestyle.min.js"></script>
-*/ ?>
+<!-- BOOTSTRAP -->
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 
 <script type="text/javascript">
 	jQuery.noConflict();
@@ -48,7 +36,6 @@
 		    jQuery("html, body").animate({ scrollTop: jQuery(scroll).offset().top }, 1000);
 		});
 	});
-
 </script>
 
 </head>
--- index.php
+++ index.php
@@ -79,19 +79,41 @@
 	<!-- contato -->
 	<section class="box-home contato">
 		<div class="container">
-			<p class="subtitulo">Você também pode enviar suas críticas, sugestões ou dúvidas preenchendo todos os campos abaixo:</p>
-			<div class="info-contato">
-				<span>CENTRAL DE RELACIONAMENTO CERAMFIX</span>
-				<h2>0800 704549</h2>
-				<a href="#">info@ceramfix.com.br</a>
-			</div>
 
-			<form action="#">
-				<fieldset>
-					<label for=""></label>
-					<input type="text" name="" id="" placeholder="">
-				</fieldset>
-			</form>
+			<div class="row">
+				<div class="col-6">
+					<p class="subtitulo">Você também pode enviar suas críticas, sugestões ou dúvidas preenchendo todos os campos abaixo:</p>
+				</div>
+
+				<div class="col-6">
+					<div class="info-contato">
+						<span>CENTRAL DE RELACIONAMENTO CERAMFIX</span>
+						<h2>0800 704549</h2>
+						<a href="#">info@ceramfix.com.br</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<form action="#" class="contato-home">
+					<fieldset class="col-12">
+						<span><input type="text" name="nome" id="nome" placeholder="Nome:"></span>
+					</fieldset>
+					<fieldset class="col-6">
+						<span><input type="text" name="email" id="email" placeholder="E-mail:"></span>
+					</fieldset>
+					<fieldset class="col-6">
+						<span><input type="text" name="" id="" placeholder="Telefone principal:"></span>
+					</fieldset>
+					<fieldset class="col-12">
+						<label for="mensagem">Mensagem:</label>
+						<textarea name="mensagem" id="mensagem" cols="30" rows="10"></textarea>
+					</fieldset>
+					<fieldset class="col-12">
+						<button class="enviar">ENVIAR!</button>
+					</fieldset>
+				</form>
+			</div>
 		</div>
 		<a href="#" class="seta" rel="body"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
 	</section>
