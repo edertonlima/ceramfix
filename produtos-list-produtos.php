ff56dc2d45d805969fcd4207e76e6b1110286ec3
Ederton Lima <contato@ederton.com.br>
2017-02-14 02:13:02 -0200

    Linha de Produtos e Listagem de Produtos

---
+++ produtos-list-produtos.php
@@ -0,0 +1,54 @@
+<?php include "includes/head.php"; ?>
+
+<body class="home">
+	<header class="header">
+		<div class="container">
+			<div class="row">
+				<?php include "includes/header.php"; ?>
+			</div>
+		</div>
+	</header>
+
+	<!-- slide -->
+	<section class="produtos">
+		<div class="container">
+			<h2>ARGAMASSA COLANTE</h2>
+			<h4>Escolha a argamassa colante<br>perfeita para sua obra</h4>
+		</div>
+
+		<img src="assets/images/produto-linha-1.jpg" class="img-linha" alt="ARGAMASSA COLANTE">
+
+		<div class="container">
+			<ul class="list-produto">
+				<li>
+					<a href="javascript:" title="ARGA FIX AC I">
+						<img src="assets/images/img-produto.jpg" alt="ARGA FIX AC I">
+						<div class="cont-list-prod">
+							<h3>ARGA FIX AC I</h3>
+							<p>Argamassa colante tipo AC I.</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="javascript:" title="ARGA FIX AC I">
+						<img src="assets/images/img-produto2.jpg" alt="ARGA FIX AC I">
+						<div class="cont-list-prod">
+							<h3>GRANDES FORMATOS AC III </h3>
+							<p></p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="javascript:" title="ARGA FIX AC I">
+						<img src="assets/images/img-produto3.jpg" alt="ARGA FIX AC I">
+						<div class="cont-list-prod">
+							<h3>ARGA FLEX AC II</h3>
+							<p>Argamassa colante tipo AC II.</p>
+						</div>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</section>	
+
+	<?php include "includes/footer.php"; ?>
